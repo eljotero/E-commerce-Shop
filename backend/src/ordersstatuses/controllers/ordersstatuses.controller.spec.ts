e9872a27c6743b3eabfaf05a1566e3dba35f76fd
eljotero <aleksander.janicki002@gmail.com>
2024-02-25 20:54:26 +0100

    [Added] Test for Ordersstatuses Controller

--- backend/src/ordersstatuses/controllers/ordersstatuses.controller.spec.ts
+++ backend/src/ordersstatuses/controllers/ordersstatuses.controller.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersstatusesController } from './ordersstatuses.controller';
+import { OrdersstatusesService } from '../services/ordersstatuses.service';
 
 describe('OrdersstatusesController', () => {
   let controller: OrdersstatusesController;
 
+  const mockOrdersStatuses = [{
+    orderStatusId: 1,
+    orderStatus: 'CANCELED'
+  }, {
+    orderStatusId: 2,
+    orderStatus: 'APPROVED'
+  }]
+
+  const mockOrdersStatusesService = {
+    findAllOrdersStatuses: jest.fn().mockResolvedValueOnce([mockOrdersStatuses])
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersstatusesController],
-    }).compile();
+      providers: [OrdersstatusesService]
+    }).overrideProvider(OrdersstatusesService).useValue(mockOrdersStatusesService).compile();
 
     controller = module.get<OrdersstatusesController>(OrdersstatusesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  describe('getAllOrdersStatuses', () => {
+    it('should return all orders statuses', async () => {
+      const result = await controller.getAllOrdersStatuses();
+      expect(mockOrdersStatusesService.findAllOrdersStatuses).toHaveBeenCalled();
+      expect(result).toEqual([mockOrdersStatuses]);
+    })
+  })
 });
